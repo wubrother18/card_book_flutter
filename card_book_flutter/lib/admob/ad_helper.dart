aaadfc7c87ce9c3e01b40a31a11f5d50197ca616
peicheng <peicheng@pyramius.com>
2024-07-10 16:14:34 +0800

    加入廣告id

--- card_book_flutter/lib/admob/ad_helper.dart
+++ card_book_flutter/lib/admob/ad_helper.dart
@@ -14,7 +14,7 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/7049598008';
+      return 'ca-app-pub-9721082206081134/3254071187';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/3964253750';
     } else {
